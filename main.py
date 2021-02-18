dd966db7b7a1b89cee662e5b37ad09bcd2279424
Haydn Gynn <haydn.gynn@gmail.com>
2021-02-18 19:09:22 +0000

    Early stage of python UI implementation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import PyQt5
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtGui, QtCore
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
 
 
 app = QtWidgets.QApplication([])
@@ -7,11 +10,45 @@ app = QtWidgets.QApplication([])
 form = uic.loadUi("menu.ui")
 
 form.show()
+DataList = {}
+
+def setClassifier(checkbox):
+    print("test")
 
 def selectFile():
-    a = QtWidgets.QFileDialog.getOpenFileName()
 
-    form.filePath.setText(a[0])
+    a = QtWidgets.QFileDialog.getOpenFileName()
+    csv = pd.read_csv(a[0])
+
+    names = a[0].split("/")
+    filename = names[len(names)-1]
+
+    DataList[filename] = csv
+    form.listWidget.addItem(filename)
+
+    a = 0
+    for col in csv.columns:
+        form.tableWidget.insertRow(form.tableWidget.rowCount())
+
+        widget = QtWidgets.QWidget()
+        checkbox = QtWidgets.QCheckBox()
+        checkbox.stateChanged.connect(lambda : setClassifier(checkbox))
+        layout = QtWidgets.QHBoxLayout(widget)
+        layout.addWidget(checkbox)
+        layout.setAlignment(QtCore.Qt.AlignCenter)
+        layout.setContentsMargins(0, 0, 0, 0)
+        widget.setLayout(layout)
+
+        form.tableWidget.setItem(a, 0, QtWidgets.QTableWidgetItem(col))
+        form.tableWidget.setCellWidget(a, 1, widget)
+
+        a += 1
+
+
+
+
+
+
 
 
 form.uploadButton.clicked.connect(selectFile)
